a6737257f504a140c9e8eb7fba5dc7d8a374d90f
Pawel80256 <paweuq2@yahoo.pl>
2022-12-05 21:54:51 +0100

    styled search teacher page

--- src/teacherComponents/searchTeacherPage/SearchTeacherPage.tsx
+++ src/teacherComponents/searchTeacherPage/SearchTeacherPage.tsx
@@ -1,13 +1,20 @@
 import {SearchTeacherForm} from "../../otherComponents/SearchTeacherForm";
 import {useAppSelector} from "../../app/hooks";
 import {TeacherList} from "../TeacherList";
+import {Divider, Grid} from "@mui/material";
 
 export const SearchTeacherPage = () => {
     const {searchedTeachers} = useAppSelector(state => state.teachers)
     return (
-        <>
+
+        <Grid sx={{display: "flex", marginTop: "4%"}} direction="column">
+            <h1 style={{margin: "auto"}}>Wyszukaj swojego nauczuciela!</h1>
             <SearchTeacherForm></SearchTeacherForm>
+            <Divider style={{marginTop: "2%", marginBottom: "2%"}}/>
+            <h2 style={{margin: "auto"}}>Wynik wyszukiwania</h2>
             <TeacherList teachers={searchedTeachers}></TeacherList>
-        </>
+        </Grid>
+
+
     )
 }
