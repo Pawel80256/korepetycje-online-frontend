21edabf7574d4d1accfbbb42ecc38cd5ca4f4922
Pawel80256 <paweuq2@yahoo.pl>
2022-12-10 16:20:07 +0100

    styled opinion element

--- src/teacherComponents/opinions/OpinionElement.tsx
+++ src/teacherComponents/opinions/OpinionElement.tsx
@@ -10,7 +10,8 @@ export interface OpinionElementProps {
 export const OpinionElement: React.FC<OpinionElementProps> = (props) => {
     const {opinion} = props
     return (
-        <Grid container direction={"row"} sx={{padding: "5px"}}>
+        <Grid container direction={"row"}
+              sx={{padding: "5px", background: "#E4F2F5", marginTop: "5px", borderRadius: "5px"}}>
 
             <Grid container item direction={"column"} style={{width: "20%"}}>
                 <Typography>
@@ -19,7 +20,7 @@ export const OpinionElement: React.FC<OpinionElementProps> = (props) => {
                 <StarRatings rating={opinion.numericValue} numberOfStars={5}
                              starRatedColor="#3990E8" starDimension={"15px"}
                              starSpacing={"1px"}/>
-                <Typography>
+                <Typography color="text.secondary" fontSize={"13px"}>
                     {new Date(opinion.createdAt).toLocaleDateString()}
                 </Typography>
             </Grid>
