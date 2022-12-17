b1dd0fee4f90c1da24b4dbba083bde4bdbb8955f
Pawel80256 <paweuq2@yahoo.pl>
2022-12-17 23:06:08 +0100

    added new register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {NavigationBar} from "./navigation/NavigationBar";
-import {RegisterForm} from "./authentication/RegisterForm";
 import {Route, Routes} from "react-router-dom";
 import {AppRoutes} from "./navigation/Routes";
 import {LoginForm} from "./authentication/LoginForm";
 import {CssBaseline} from "@mui/material";
 import {SearchTeacherPage} from "./teacherComponents/searchTeacherPage/SearchTeacherPage";
 import {TeacherPublicProfile} from "./teacherComponents/teacherPublicProfile/TeacherPublicProfile";
+import {NewRegisterForm} from "./authentication/NewRegisterForm";
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
             <CssBaseline/>
             <Routes>
                 <Route path={AppRoutes.HOME} element={<></>}/>
-                <Route path={AppRoutes.REGISTER} element={<RegisterForm/>}/>
+                <Route path={AppRoutes.REGISTER} element={<NewRegisterForm/>}/>
                 <Route path={AppRoutes.LOGIN} element={<LoginForm/>}/>
                 <Route path={AppRoutes.SEARCH} element={<SearchTeacherPage/>}/>
                 <Route path={AppRoutes.TEACHER_PUBLIC_PROFILE} element={<TeacherPublicProfile/>}/>
